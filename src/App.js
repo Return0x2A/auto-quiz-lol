875b5e0dde13a186e33ef313f607f86aa60c9ffa
fairydhwen <fairydhwen@gmail.com>
2022-06-09 22:54:31 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import "./index.css";
+
+export default function App() {
+  return (
+    <>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+    </>
+  );
+}
